27ec64ee348071f5ecc95febfbdee7708ea81a97
bhargav raut <bhargav.r.raut@gmail.com>
2019-04-22 10:20:19 +0530

    added redis configuration for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,7 @@
-cnfg = nil
-REDIS_CONFIG = YAML.load( File.open( Rails.root.join("config/redis.yml") ) ).symbolize_keys
-dflt = REDIS_CONFIG[:default].symbolize_keys
-cnfg = dflt.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
-$redis = Redis.new(cnfg)
-# To clear out the db before each test
-puts "FLUSHING REDIS DB SINCE ENV IS DEVELOPMENT."
-$redis.flushdb if Rails.env = "development"
+redis_host = ENV["REDIS_URL"] || "localhost:6379"
+$redis = Redis.new(url: ENV["REDIS_URL"])
+$redis.set("first_key","first_value")
+if Rails.env.development?
+	puts "flushing db as we are in development."
+	$redis.flushdb
+end
