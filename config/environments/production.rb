acd3906658692f171ae9867726e4754874034c76
bhargav raut <bhargav.r.raut@gmail.com>
2019-04-29 16:45:13 +0530

    changed the host for mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,10 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  #config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
