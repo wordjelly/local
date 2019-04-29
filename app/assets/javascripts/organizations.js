311117c12aeaee3dc20021d6b9da899570ee05a1
bhargav raut <bhargav.r.raut@gmail.com>
2019-04-29 18:24:03 +0530

    creates all indices on startup

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -1,4 +1,3 @@
-/***
 function remove_verified_user_id(user_id){
 
 }
@@ -14,7 +13,7 @@ function remove_rejected_user_id(user_id){
 function add_rejected_user_id(user_id){
 
 }
-***/
+
 $(document).on('click','.accept_user',function(event){
 	$(this).parent().append("<input type='hidden' name='organization[user_ids][] value='" + $(this).attr("data-user-id") + "' />");
 	$("<i class='large material-icons'>check</i>").insertBefore($(this));
@@ -22,5 +21,5 @@ $(document).on('click','.accept_user',function(event){
 
 $(document).on('click','.reject_user',function(event){
 	$(this).parent().append("<input type='hidden' name='organization[rejected_user_ids][] value='" + $(this).attr("data-user-id") + "' />");	
-	$(("<i class='large material-icons'>check</i>").insertBefore($(this));
+	$("<i class='large material-icons'>check</i>").insertBefore($(this));
 });
